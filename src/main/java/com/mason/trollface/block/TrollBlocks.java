b2c5ec94274022764692e2ba2b9c794ccaa6d621
moist-mason <67809772+moist-mason@users.noreply.github.com>
2022-08-26 15:04:56 -0500

    Trollface block loot drop + hardness adjustment

--- src/main/java/com/mason/trollface/block/TrollBlocks.java
+++ src/main/java/com/mason/trollface/block/TrollBlocks.java
@@ -19,11 +19,12 @@ public class TrollBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, Trollface.MOD_ID);
 
+    // When I initially implemented the trollface block, it only had a strength of 9f, but because I hadn't set the pickaxe/needs_pickaxe json files yet, it took way longer to break.
+    // I've set up those json files, but I wanted to keep the toughness of the block, because a block that's annoyingly difficult to break is funny.
+    // With a hardness/strength value of 70, the trollface block is tougher than any block in the vanilla game. That's a lot of "blocks". -Mason
     public static final RegistryObject<Block> TROLLFACE_BLOCK = registerBlock("trollface_block",
             () -> new Block(Block.Properties.of(Material.METAL)
-                    .strength(7.5f).requiresCorrectToolForDrops()), TrollCreativeTab.TAB_TROLLFACE);
-    // The Block of Trollface (trollface_block) is the hardest (breakable) material in the game. It was actually a lot harder when I initially put it in,
-    // and I dialed it back when I realized how hard 9f truly was. I still decided that it should be hard though, because an annoyingly tough block is pretty funny. -Mason
+                    .strength(70f).requiresCorrectToolForDrops()), TrollCreativeTab.TAB_TROLLFACE);
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
