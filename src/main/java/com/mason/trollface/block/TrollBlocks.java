d10ff96eabc35ffc111736ab337444ff301cd343
moist-mason <67809772+moist-mason@users.noreply.github.com>
2022-09-22 18:36:45 -0500

    - removed block of trollface lore

--- src/main/java/com/mason/trollface/block/TrollBlocks.java
+++ src/main/java/com/mason/trollface/block/TrollBlocks.java
@@ -19,9 +19,6 @@ import java.util.function.Supplier;
 public class TrollBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Trollface.MOD_ID);
 
-    /* When I initially implemented the trollface block, it only had a strength of 9f, but because I hadn't set the pickaxe/needs_pickaxe json files yet, it took way longer to break.
-     * I've set up those json files, but I wanted to keep the toughness of the block, because a block that's annoyingly difficult to break is funny.
-     * With a hardness/strength value of 70, the trollface block is tougher than any block in the vanilla game. That's a lot of "blocks". -Mason */
     public static final RegistryObject<Block> TROLLFACE_BLOCK = registerBlock("trollface_block", () -> new BlockofTrollface(Block.Properties.of(Material.METAL).strength(70f).requiresCorrectToolForDrops()), TrollCreativeTab.TAB_TROLLFACE);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
